d4b9ad43d7cc827e88936ced4c2eb0a154eb3db8
tarosjk <tarosjk@gmail.com>
2023-02-28 11:01:36 +0900

    固定ページの作成

---
+++ wp-content/themes/bistrocalme/page.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+
+	<h2 class="pageTitle">わたしたちについて<span>ABOUT</span></h2>
+
+	<main class="main">
+		<div class="container">
+			<div class="content">
+				<figure>
+					<img src="./assets/img/page/about-1.jpg" alt="">
+				</figure>
+				<p>BISTRO CALME はソムエリのいるカジュアルなワインバーよりなビストロです。</p>
+				<p>野菜、肉、魚を食材とした月替わりの各種料理とデザートをご提供しております。</p>
+			</div>
+		</div>
+	</main>
+
+<?php get_footer(); ?>
